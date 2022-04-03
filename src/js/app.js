bf734e9387b0f1101f7323d0710dd1a29a68c03f
coniYkevin <102885556+coniYkevin@users.noreply.github.com>
2022-04-02 22:16:08 -0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+window.onload = () =>{
+    let fechaFinal = new Date("Aug 19, 2022 17:30:00").getTime();
+
+    let tiempo = setInterval(()=>{
+        let ahora = new Date().getTime();
+        let restante = fechaFinal - ahora;
+
+        let dias = Math.floor(restante / (1000 * 60 * 60 * 24));
+        let horas = Math.floor((restante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutos = Math.floor((restante % (1000 * 60 * 60)) / (1000 * 60));
+        let segundos = Math.floor((restante % (1000 * 60)) / 1000);
+
+        let divdias = document.getElementById("dias");
+        let divhoras = document.getElementById("horas");
+        let divminutos = document.getElementById("minutos");
+        let divsegundos = document.getElementById("segundos");
+
+        divdias.textContent = dias;
+        divhoras.textContent = horas;
+        divminutos.textContent = minutos;
+        divsegundos.textContent = segundos;
+    }, 1000);
+    
+}
